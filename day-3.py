4b9086a97c12ed4973c67c356d627bedb881bcab
vishal sobani <vishal.sobani@gmail.com>
2019-09-06 10:41:56 +0530

    Strings

---
+++ day-3.py
@@ -0,0 +1,17 @@
+the_string = 'some3'
+length=12
+input=len(the_string)
+times=length//input
+rem=length%input
+print(the_string*times+the_string[0:2])
+
+print(the_string[1:])
+print(the_string[-2:])
+print(the_string[:])
+print(the_string[:1])
+print(the_string[:-2])
+print(the_string[:0])
+print(the_string[:--2])
+print(the_string[:12])
+print(the_string[-1:1])
+print(the_string[-1:0])
