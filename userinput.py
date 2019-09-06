1d8f7a156ff3c63833f299da24a8e8e8c6796179
vishal sobani <vishal.sobani@gmail.com>
2019-09-06 14:03:48 +0530

    userinputs

--- userinput.py
+++ userinput.py
@@ -1,15 +1,18 @@
-"""
+
 
 first_name= str(input("Enter your name :"))
 last_name= str(input("Enter your last name : "))
 salary=int(input("enter your salary :"))
 age=int(input("enter your age :"))
-print(f"my nasdme is {first_name} and my salary is {salary}")"""
+print(f"my name is {first_name} and my salary is {salary}")
+
+
+
 
 student_name=str(input("enter the student name "))
 student_age=int(input("enter the age of student "))
 student_class=str(input("enter the class "))
-physvisics_score=int(input("enter the score in physics "))
+physics_score=int(input("enter the score in physics "))
 chemistry_score=int(input("enter the score in chemistry "))
 biology_score=int(input("enter the score in biology "))
 social=int(input("enter the score in social "))
@@ -17,4 +20,4 @@ english=int(input("score in english "))
 hindi=int(input("score in hindi "))
 total_marks=physics_score+biology_score+chemistry_score+english+hindi+social
 average_scored=(total_marks/600)*100
-print(f"{student_name} has scored {average_scored} % in exam")
+print(f"{student_navishalme} has scored {average_scored} % in exam")
