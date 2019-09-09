87f6fccd70b7f71d7235c7aa9c59c4771b8ae58e
vishal sobani <vishal.sobani@gmail.com>
2019-09-09 09:51:02 +0530

    adding ageinput with IF-ELSE

---
+++ ageinput.py
@@ -0,0 +1,11 @@
+age_input=int(input("Enter the age "))
+
+if age_input>18:
+    print("got to college")
+
+print("got to school")
+
+if age_input>18:
+    print("you have to vote")
+else:
+    print("you cant vote")
