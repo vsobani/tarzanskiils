ed0cdc221497efe715c9e21e410e4c9e8ea3cec2
vishal sobani <vishal.sobani@gmail.com>
2019-09-09 14:39:58 +0530

    while loop working programs

--- whileloop.py
+++ whileloop.py
@@ -1,31 +1,36 @@
-# even=int(input("Enter an integer from 1 to 50 "))
-# while even<=50:
-#     if even%2==0:
-#         print(f"The even numbers are",even)
-#     even=even+1
-#
-#
-#
-# input_integer=int(input("Enter the integer "))
-# i=0
-# while i<input_integer:
-#     a=i**2
-#     i=i+1
-#     print("square of integers are",a)
+"""This is printing of even integers from 1 to 50 """
 
-# i=0
-# sum=0
-# while i<9:
-#     input_key =int( input("Enter the integers "))
-#     sum=sum+input_key
-#     i=i+1
-#     average=sum/10
-# print("the average of intergers are",average)
-# i=0
-# while i<4:
-#     i = i + 1
-#     print(i*"*")
-# print(" ",end='')
+even=int(input("Enter an integer from 1 to 50 "))
+while even<=50:
+    if even%2==0:
+        print(f"The even numbers are",even)
+    even=even+1
+
+"""This is printing square of a number"""
+
+input_integer=int(input("Enter the integer "))
+square=0
+while square<input_integer:
+    num=square**2
+    square=square+1
+    print(f"square of integers {num} are",num)
+
+
+"""This is printing of pattern"""
+
+i=0
+sum=0
+while i<9:
+    input_key =int( input("Enter the integers "))
+    sum=sum+input_key
+    i=i+1
+    average=sum/10
+print("the average of intergers are",average)
+i=0
+while i<4:
+    i = i + 1
+    print(i*"*")
+print(" ",end='')
 
 line_count=int(input("Enter the line count "))
 current_line=1
@@ -34,8 +39,7 @@ while current_line<line_count:
     if current_line%2!=0:
         print(current_line*"*")
     current_line=current_line+1
-#print (current_line)
-#current_line=current_line-1
+
 while current_line>=1:
     if current_line%2!=0:
         print(current_line*"*")
