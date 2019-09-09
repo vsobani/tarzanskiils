607f912627b03a80178f5e9311ab7e89a01c0cdf
vishal sobani <vishal.sobani@gmail.com>
2019-09-09 11:07:27 +0530

    day-5 if elif else condition problems

--- agecheck.py
+++ agecheck.py
@@ -1,7 +1,7 @@
 age_input=int(input("Enter the age "))
 
 if 2<=age_input<=5:
-    print("got to kinddergaden")
+    print("go to kindergarten")
 elif 6<=age_input<=13:
     print("go to secondary school")
 elif 14<=age_input<=16:
@@ -20,6 +20,8 @@ elif 6>=n<=20:
     print("wierd")
 elif (n%2==0 or n>20):
     print("not wired")
+else:
+    print("print the input",n)
 
 score=int(input("enter the score "))
 if 25 < score <= 30:
@@ -36,3 +38,26 @@ elif 0 < score <= 5:
     print("You have scored F grade")
 else:
     print("You have crossed the input")
+
+take_int=int(input("enter the integer "))
+
+if take_int==1:
+    print("one")
+elif take_int==2:
+    print("two")
+elif take_int==3:
+    print("three")
+elif take_int==4:
+    print("four")
+elif take_int==5:
+    print("five")
+elif take_int==6:
+    print("six")
+elif take_int==7:
+    print("seven")
+elif take_int==8:
+    print("eight")
+elif take_int >9:
+    print("Greater than nine")
+else:
+    print("The value should be in integer")
